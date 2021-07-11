bf8cbbe3f701ebcf5a5fff10e50958767e5f1a52
Ayer Ulas Keskin <ayberulaskeskin@gmail.com>
2021-07-11 03:31:34 +0300

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+ [SerializeField] private float _speed;
+ [SerializeField] private FloatingJoystick _joystick;
+ private Rigidbody _rigidbody;
+
+ private void Awake()
+ {
+  _rigidbody = GetComponent<Rigidbody>();
+ }
+
+ private void FixedUpdate()
+ {
+  Vector3 direction = Vector3.forward * _joystick.Vertical + Vector3.right * _joystick.Horizontal;
+  _rigidbody.velocity=direction*(  _speed * Time.fixedDeltaTime);
+ }
+}
